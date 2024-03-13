febf523aa1fa2f3e467f014788f654c5d999b83b
Th1234wee <theara6574@gmail.com>
2024-03-13 18:21:35 +0700

    second commit

--- function.php
+++ function.php
@@ -1,3 +1,5 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <?php 
     $connection = new mysqli('localhost','root','','db6-7',3307);
 
@@ -16,8 +18,47 @@
                 $result     = $connection -> query($sql_insert);
 
                 if($result){
-                    echo "<h1>Success</h1>";
+                    echo '
+                            <script>
+                                $(document).ready(function(){
+                                    swal({
+                                        title: "Inserting Success",
+                                        text: "You inserted data successfully",
+                                        icon: "success",
+                                        button: "Aww yiss!",
+                                      });
+                                })
+                            </script>
+                    ';
                 }
+                else{
+                    echo '
+                            <script>
+                                $(document).ready(function(){
+                                    swal({
+                                        title: "Something went wrong",
+                                        text: "Cannot insert data",
+                                        icon: "error",
+                                        button: "Confirm",
+                                      });
+                                })
+                            </script>
+                    ';
+                }
+            }
+            else{
+                echo '
+                        <script>
+                            $(document).ready(function(){
+                                swal({
+                                    title: "Invaild Data",
+                                    text: "All field must be filled",
+                                    icon: "error",
+                                    button: "Confirm",
+                                  });
+                            })
+                        </script>
+                ';
             }
         }
     }
